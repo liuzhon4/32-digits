33f4b5388d5dff3df3f2cd8c7d537636b3afa132
liuzhon4 <jmtc_nz@hotmail.com>
2017-11-11 19:01:11 +0800

    finish distribution date

--- app/src/main/java/com/example/edward/a32_digits/MainActivity.java
+++ app/src/main/java/com/example/edward/a32_digits/MainActivity.java
@@ -11,18 +11,42 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
-    private EditText mTextView;
+    private EditText mFirst16Text;
+    private EditText mLast16Text;
+    private TextView mDateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.button);
-        mTextView = findViewById(R.id.digitInputView);
+        mFirst16Text = findViewById(R.id.first16View);
+        mLast16Text = findViewById(R.id.last16View);
+        mDateView = findViewById(R.id.dateView);
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, mTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+                String first16 = mFirst16Text.getText().toString();
+                String last16 = mLast16Text.getText().toString();
+                String distributionDate = setDistributionDate(first16);
+                Toast.makeText(MainActivity.this, getInfo(first16, last16),
+                        Toast.LENGTH_SHORT).show();
+                mDateView.setText(distributionDate);
+
             }
         });
+    }
+
+    public String getInfo(String first16, String last16) {
+        return first16 + last16;
+    }
+
+    public String setDistributionDate(String first16) {
+        String date = first16.substring(0, 5);
+        String year = date.substring(0, 1);
+        String month = date.substring(1, 3);
+        String day = date.substring(3);
+        StringBuilder sb = new StringBuilder();
+        return sb.append("201" + year + "." + month + "." + day).toString();
+
     }
 }
